10bcf0e0bb2e58166fce3c1e24782f455294ddc6
ricmig <ricmig@hotmail.com>
2019-07-03 15:52:23 -0300

    mudança na seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,6 +21,12 @@ class DatabaseSeeder extends Seeder
             'email' => Str::random(10).'@gmail.com',
             'password' => bcrypt('secret'),
         ]);
+        DB::table('users')->insert([
+            'name' => 'Ricardo',
+            'email' => 'ricmig@hotmail.com',
+            'password' => 1234
+        ]);
+        // receita 1
         DB::table('receitas')->insert([
             'id_user' => 1,
             'titulo' => 'Carne Assada',
@@ -47,8 +53,9 @@ class DatabaseSeeder extends Seeder
             'nome_ingrediente' => 'Batata',
             'quantidade' => '1 kg',
         ]);
+        // receita 2
         DB::table('receitas')->insert([
-            'id_user' => 2,
+            'id_user' => 1,
             'titulo' => 'Frango Cozido com açafrão',
             'modo_de_preparo' => 'Aqueça a panela de pressão, acrescente o óleo, o frango com o molho do tempero e tampe a panela.',
             'categoria' => 2,
@@ -73,6 +80,7 @@ class DatabaseSeeder extends Seeder
             'nome_ingrediente' => 'Requeijão',
             'quantidade' => '3 colheres',
         ]);
+        // receita 3
         DB::table('receitas')->insert([
             'id_user' => 1,
             'titulo' => 'Peixe ao Forno',
@@ -99,27 +107,29 @@ class DatabaseSeeder extends Seeder
             'nome_ingrediente' => 'Farinha de Rosca',
             'quantidade' => '200 gramas',
         ]);
+        // receita 4
         DB::table('receitas')->insert([
-            'id_user' => 2,
+            'id_user' => 1,
             'titulo' => 'Penne ao sugo e manjericão',
             'modo_de_preparo' => 'Refogue a cebola e o azeite, coloque a tomate e o açúcar. Deixe o molho ficar grosso e coloque o molho de tomate, acrescente o sal e salsa.',
             'categoria' => 4,
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 5,
+            'id_receita' => 4,
             'nome_ingrediente' => 'Macarrão tipo penne',
             'quantidade' => '1 pacote',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 5,
+            'id_receita' => 4,
             'nome_ingrediente' => 'Manjericão',
             'quantidade' => '2 colhares de sopa',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 5,
+            'id_receita' => 4,
             'nome_ingrediente' => 'Molho de tomate',
             'quantidade' => 'a gosto',
         ]);
+        // receita 5
         DB::table('receitas')->insert([
             'id_user' => 1,
             'titulo' => 'Carne de churrasco',
@@ -127,43 +137,44 @@ class DatabaseSeeder extends Seeder
             'categoria' => 1,
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 6,
+            'id_receita' => 5,
             'nome_ingrediente' => 'Fraldinha',
             'quantidade' => '1 kg',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 6,
+            'id_receita' => 5,
             'nome_ingrediente' => 'Sal Grosso',
             'quantidade' => '1 ',
         ]);
+        // receita 6
         DB::table('receitas')->insert([
-            'id_user' => 3,
+            'id_user' => 1,
             'titulo' => 'Picanha com molho branco',
             'modo_de_preparo' => 'Derreta a manteiga em fogo baixo e junte o leite e a farinha misturados até o ponto de molho branco',
             'categoria' => 1,
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 7,
+            'id_receita' => 6,
             'nome_ingrediente' => 'Medalhões de Picanha',
             'quantidade' => '500 gramas',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 7,
+            'id_receita' => 6,
             'nome_ingrediente' => 'Requeijão',
             'quantidade' => '2 colheres',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 7,
+            'id_receita' => 6,
             'nome_ingrediente' => 'Leite',
             'quantidade' => '1 litro ',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 7,
+            'id_receita' => 6,
             'nome_ingrediente' => 'Queijo parmesão',
             'quantidade' => '300 gramas',
         ]);
         DB::table('ingredientes')->insert([
-            'id_receita' => 7,
+            'id_receita' => 6,
             'nome_ingrediente' => 'Creme de 4 queijos',
             'quantidade' => '40 gramas',
         ]);                   
